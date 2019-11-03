2c216517c007a52ac5dc656356b65ab0f75cb54b
Daniel Belarmino <belarba@gmail.com>
2019-11-02 21:56:40 -0300

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Client.destroy_all
+Call.destroy_all
+
+puts 'Creating clients...' 
+client = Client.new(phone: "21998366200")
+client.save!
+
+puts 'Creating calls...'
+call1 = Call.new(phone: "21998540031", start: Time.strptime('02/29/2017 12:00', '%m/%d/%Y %H:%M'), end: Time.strptime('02/29/2017 14:00', '%m/%d/%Y %H:%M'), client: client)
+call1.save!
+
+call2 = Call.new(phone: "21998540031", start: Time.strptime('12/11/2018 15:07', '%m/%d/%Y %H:%M'), end: Time.strptime('12/11/2018 15:14', '%m/%d/%Y %H:%M'), client: client)
+call2.save!
+
+call3 = Call.new(phone: "21998540031", start: Time.strptime('12/12/2018 22:47', '%m/%d/%Y %H:%M'), end: Time.strptime('12/12/2018 22:50', '%m/%d/%Y %H:%M'), client: client)
+call3.save!
+
+call4 = Call.new(phone: "21998540031", start: Time.strptime('12/12/2018 21:57', '%m/%d/%Y %H:%M'), end: Time.strptime('12/12/2018 22:10', '%m/%d/%Y %H:%M'), client: client)
+call4.save!
+
+call5 = Call.new(phone: "21998540031", start: Time.strptime('12/12/2018 04:57', '%m/%d/%Y %H:%M'), end: Time.strptime('12/12/2018 06:10', '%m/%d/%Y %H:%M'), client: client)
+call5.save!
+
+call6 = Call.new(phone: "21998540031", start: Time.strptime('12/13/2018 21:57', '%m/%d/%Y %H:%M'), end: Time.strptime('12/14/2018 22:10', '%m/%d/%Y %H:%M'), client: client)
+call6.save!
+
+call7 = Call.new(phone: "21998540031", start: Time.strptime('12/12/2018 15:07', '%m/%d/%Y %H:%M'), end: Time.strptime('12/12/2018 15:12', '%m/%d/%Y %H:%M'), client: client)
+call7.save!
+
+call8 = Call.new(phone: "21998540031", start: Time.strptime('02/28/2019 21:57', '%m/%d/%Y %H:%M'), end: Time.strptime('03/01/2019 22:10', '%m/%d/%Y %H:%M'), client: client)
+call8.save!
